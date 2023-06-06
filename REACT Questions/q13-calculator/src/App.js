954e26bc1b102aa55ac6cbc677dd0a6e8aad0ddc
Yashoda <yashodalakshmi999@gmail.com>
2023-06-06 11:14:52 +0530

    calculator

---
+++ REACT Questions/q13-calculator/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './style.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeyPadComponent';
+
+class App extends Component {
+  state = {
+    result: ""
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate();
+    }
+
+    else if(button === "C") {
+      this.reset();
+    }
+
+    else if(button === "CE") {
+      this.backspace();
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+  calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+')
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    } catch(e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Simple Calculator App using REACT</h1>
+        <div className="calculator-body">
+          <ResultComponent result={this.state.result} />
+          <KeyPadComponent onClick={this.onClick} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
